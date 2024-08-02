9ae112ff81d2f52479dbb39906f1d7625a30bcbc
forwardtop <noproblemgod@hotmail.com>
2024-08-02 19:43:34 +0800

    changed branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import MultiSectionForm from "./MultiSectionForm";
+
+function App() {
+  return (
+    <div>
+      <MultiSectionForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client"; // Updated import
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#00294b", // Blue color for primary
+    },
+    secondary: {
+      main: "#000000", // Black color for secondary
+    },
+  },
+  typography: {
+    fontFamily: "Arial", // Use Arial font
+  },
+});
+
+// Create a root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the application
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
