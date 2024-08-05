68c89cf27824e00ec1b9a39a2f0ffdfff1e1109c
forwardtop <noproblemgod@hotmail.com>
2024-08-06 03:31:07 +0800

    fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-// src/App.js
-
 import React from "react";
-import AppRoutes from "./route";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ClientTable from "./components/ClientTable";
+import MultiSectionForm from "./components/MultiSectionForm";
+import { Container } from "@mui/material";
 
-function App() {
-  return <AppRoutes />;
-}
+const App = () => (
+  <Router>
+    <Container>
+      <Routes>
+        <Route exact path="/" element={<ClientTable />} />
+        <Route path="/client_form/new" element={<MultiSectionForm />} />
+        <Route path="/client_form/edit/:id" element={<MultiSectionForm />} />
+      </Routes>
+    </Container>
+  </Router>
+);
 
 export default App;
--- src/route.js
+++ src/route.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ClientTable from "./components/ClientTable"; // Assuming this is the component with your customer table
-import MultiSectionForm from "./components/MultiSectionForm"; // Assuming this is the form component
-import { Container } from "@mui/material";
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import ClientTable from "./components/ClientTable"; // Assuming this is the component with your customer table
+// import MultiSectionForm from "./components/MultiSectionForm"; // Assuming this is the form component
+// import { Container } from "@mui/material";
 
-const AppRoutes = () => (
-  <Router>
-    <Container>
-      <Routes>
-        <Route exact path="/" element={<ClientTable />} />
-        <Route path="/client_form/new" element={<MultiSectionForm />} />
-        <Route path="/client_form/edit/:id" element={<MultiSectionForm />} />
-      </Routes>
-    </Container>
-  </Router>
-);
+// const AppRoutes = () => (
+//   <Router>
+//     <Container>
+//       <Routes>
+//         <Route exact path="/" element={<ClientTable />} />
+//         <Route path="/client_form/new" element={<MultiSectionForm />} />
+//         <Route path="/client_form/edit/:id" element={<MultiSectionForm />} />
+//       </Routes>
+//     </Container>
+//   </Router>
+// );
 
-export default AppRoutes;
+// export default AppRoutes;
